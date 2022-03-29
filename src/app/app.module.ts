6813ebd02dafb108f06f8be244e839efca1b5398
Candela Neil <caianeil27@gmail.com>
2022-03-29 19:52:16 -0300

    g

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AppNavbarComponent } from './app-navbar/app-navbar.component';
+import { AppNavbarComponent } from './componentes/app-navbar/app-navbar.component';
+import { SobreMiComponent } from './componentes/sobre-mi/sobre-mi.component';
+import { ExperienciaLaboralComponent } from './componentes/experiencia-laboral/experiencia-laboral.component';
+import { EstudiosRealizadosComponent } from './componentes/estudios-realizados/estudios-realizados.component';
+import { ContactameComponent } from './componentes/contactame/contactame.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppNavbarComponent
+    AppNavbarComponent,
+    SobreMiComponent,
+    ExperienciaLaboralComponent,
+    EstudiosRealizadosComponent,
+    ContactameComponent
   ],
   imports: [
     BrowserModule,
